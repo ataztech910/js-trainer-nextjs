793305d24a31e7e798c453e899151e1d79024be4
ataztech910 <webconsult.ekb@gmail.com>
2023-12-06 02:06:25 +0100

    update page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: false,
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/:path*",
+                headers: [
+                    { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+                    { key: "Cross-Origin-Opener-Policy", value: "same-origin" }
+                ]
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
