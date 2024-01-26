90ef2e61a4cc717cbab6ba029e6e4b3e58a20221
ataztech910 <webconsult.ekb@gmail.com>
2024-01-26 11:18:19 +0100

    remove bot

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,16 +1,16 @@
-import { NextResponse } from "next/server";
-import { bot } from "../bot";
+// import { NextResponse } from "next/server";
+// import { bot } from "../bot";
 
-export async function GET(request) {
-    return NextResponse.json({ message: "Hello World" }, { status: 200 });
-}
+// export async function GET(request) {
+//     return NextResponse.json({ message: "Hello World" }, { status: 200 });
+// }
 
-export async function POST(request) {
-    try {
-        bot.processUpdate(await request.json());
-        return new Response("", { status: 200 });
-    } catch (e) {
-        console.error(e);
-        return new Response("", { status: 500 });
-    }
-}
+// export async function POST(request) {
+//     try {
+//         bot.processUpdate(await request.json());
+//         return new Response("", { status: 200 });
+//     } catch (e) {
+//         console.error(e);
+//         return new Response("", { status: 500 });
+//     }
+// }
--- src/app/bot.js
+++ src/app/bot.js
@@ -1,15 +1,24 @@
-import TelegramBot from "node-telegram-bot-api";
+// import TelegramBot from "node-telegram-bot-api";
 
-const BOT_TOKEN = '939804369:AAEw200332QILe8MdVtqrqVrsuJwT05hY3I';
-export const bot = new TelegramBot(BOT_TOKEN);
-console.log("bot", BOT_TOKEN);
+// const BOT_TOKEN = '939804369:AAEw200332QILe8MdVtqrqVrsuJwT05hY3I';
+// export const bot = new TelegramBot(BOT_TOKEN);
+// console.log("bot", BOT_TOKEN);
 
-export const history = [];
-bot.on("message", async (msg) => {
-    try {
-        await bot.sendMessage(msg.chat.id, msg.text + ", and?");
-        history.push({ when: new Date(), msg });
-    } catch (e) {
-        console.error(e);
-    }
-});
+// export const history = [];
+// bot.on("message", async (msg) => {
+//     try {
+//         await bot.sendMessage(msg.chat.id, msg.text + ", and?");
+//         history.push({ when: new Date(), msg });
+//     } catch (e) {
+//         console.error(e);
+//     }
+// });
+
+/**
+ * 
+ * 
+ * curl -X POST https://api.telegram.org/bot939804369:AAEw200332QILe8MdVtqrqVrsuJwT05hY3I/setWebhook \
+    -H "Content-type: application/json" \
+    -d '{"url": "https://js-trainer-nextjs.vercel.app/api"}'
+
+ */
